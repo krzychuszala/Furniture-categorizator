f7f82658ef8f56ced2c988f4e63dc17c4ac99264
krzychuszala <59472045+krzychuszala@users.noreply.github.com>
2025-08-20 14:17:06 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -97,7 +97,8 @@ if not os.path.exists(MODEL_PATH):
     url = f"https://drive.google.com/uc?id={file_id}"
     gdown.download(url, MODEL_PATH, quiet=False)
 
-model.load_state_dict(torch.load("efficientnet_v2_s_fc_layer3_layer4.pth", map_location='cpu'))
+model = load_efficientnet_v2_s(num_classes=len(class_map))
+model.load_state_dict(torch.load("efficientnet_v2_s_fc_layer3_layer4.pth"))
 
 model.eval()
 
